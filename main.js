e4f5202eb660e2c569f093251df4c3bb62d03e9b
Scott T Lyman <s.t.lyman84@gmail.com>
2022-02-08 16:52:45 -0700

    build complete, bug in  js:18

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const packages = [
   { missing: false, heavy: false, priority: true, fragile: true, to: 'Mark', trackingNumber: '1325sdk' },
   { missing: false, heavy: true, priority: false, fragile: true, to: 'Mick', trackingNumber: 'jffd147' },
   { missing: false, heavy: false, priority: false, fragile: false, to: 'Jake', trackingNumber: 'acdc145' },
-  { missing: false, heavy: true, priority: true, fragile: true, to: 'Brittany' },
+  { missing: false, heavy: true, priority: true, fragile: true, to: 'Brittany', trackingNumber: '' },
   { missing: false, heavy: false, priority: true, fragile: false, to: 'Zach', trackingNumber: '8081baz' },
   { missing: false, heavy: true, priority: false, fragile: true, to: 'Jeremy', trackingNumber: 'suz2367' }]
+
+let current = []
 
 function draw() {
   let table = ''
   for (let i = 0; i < packages.length; i++) {
-    const package = packages[i]
+    const package = current[i]
     table += `
     <div class="row justify-space-between mt-2 tableRow">
     <p class="col fw-bold">${package.to}</p>
@@ -24,6 +26,7 @@ function draw() {
   document.getElementById('packList').innerHTML = table
 }
 
+
 function missPack() {
   packages.forEach(package => package.missing = false)
   const randIndex = Math.floor(Math.random() * packages.length)
@@ -32,3 +35,19 @@ function missPack() {
   draw()
 }
 
+function filter(filtProp) {
+  let lostPackage = packages.find(package => package.missing)
+  current = current.filter(package => package[filtProp] === lostPackage[filtProp])
+  draw()
+}
+
+function guess(to) {
+  let lostPackage = packages.find(package => package.missing)
+  if (lostPackage.to == to) {
+    alert('Package FOUND!')
+  } else {
+    alert("You couldn't find the package :(")
+  }
+}
+
+missPack()
